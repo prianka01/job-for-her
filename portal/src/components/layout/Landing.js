712d855fd086ed9587d1f463988a8864f8eeefd1
Priyanka Jalan <prijalan2001@gmail.com>
2022-07-10 17:59:59 +0530

    Updated UI and add get profile data

--- portal/src/components/layout/Landing.js
+++ portal/src/components/layout/Landing.js
@@ -4,70 +4,80 @@ import { Link } from "react-router-dom";
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div style={{ height: "80vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
+            {/* <h4>
               <b>Build</b> a login/auth app with the{" "}
               <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
               scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
+            </h4> */}
+            {/* <p className="flow-text grey-text text-darken-1">
               Create a (minimal) full-stack app with user authentication via
               passport and JWTs
+            </p> */}
+            <p className="flow-text grey-text text-darken-1">
+              Platform to connect women after career break to reskilling and
+              mapping jobs
             </p>
             <br />
             <div className="col s6">
-              <Link
-                to="/registeruser"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                RegisterUser
-              </Link>
+              <h4>USERS</h4>
+              <div className="row s6">
+                <Link
+                  to="/registeruser"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                  }}
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Register
+                </Link>
+              </div>
+              <div className="row s6">
+                <Link
+                  to="/loginuser"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                  }}
+                  className="btn btn-large btn-flat waves-effect white black-text"
+                >
+                  LogIn
+                </Link>
+              </div>
             </div>
             <div className="col s6">
-              <Link
-                to="/loginuser"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                LogIn User
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
-                to="/registerorg"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register Organization
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
-                to="/loginorg"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                LogIn Organization
-              </Link>
+              <h4>ORGANIZATIONS</h4>
+              <div className="row s6">
+                <Link
+                  to="/registerorg"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                  }}
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Register
+                </Link>
+              </div>
+              <div className="row s6">
+                <Link
+                  to="/loginorg"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                  }}
+                  className="btn btn-large btn-flat waves-effect white black-text"
+                >
+                  LogIn
+                </Link>
+              </div>
             </div>
           </div>
         </div>
